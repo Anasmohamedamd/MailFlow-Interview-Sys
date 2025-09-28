cd67fe763591cb7b6ae7ed076a31d90a74b5a3f5
Anas Mohamed <anasmohamed1016@gmail.com>
2025-09-29 02:30:21 +0530

    Update axiosInstance.js

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -16,28 +16,5 @@
    return config;
  });
 
-// axiosInstance.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL, 
-// });
-
-// // Attach token automatically
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-
-
 export default axiosInstance;
 
